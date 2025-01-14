c0ae6d86448e45c33dbe65aeb49e6f888fcfbe69
sifat1234 <ahahmedsifat111@gmail.com>
2025-01-14 23:05:05 +0600

    useState Types

---
+++ 12. TypeScipt With Nextjs/4. useState Types/app/page.tsx
@@ -0,0 +1,27 @@
+import UsersInfo from '@/components/UserInfo';
+import AdminInfo from '@/components/AdminInfo';
+import Counter from '@/components/Counter';
+
+export default function Home() {
+  return (
+    <>
+      <h1>Users Info 👇</h1>
+      <UsersInfo
+        username='Alex'
+        email='alex@gmail.com'
+        age={20}
+        location={['Earth', 'USA']}
+      />
+
+      <h1>Admin Info 👇</h1>
+      <AdminInfo
+        username='Alex'
+        email='alex@gmail.com'
+        age={20}
+        location={['Mars', 'Unknown']}
+        admin='yes'
+      />
+      <Counter />
+    </>
+  );
+}
